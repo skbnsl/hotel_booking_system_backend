01915042e86017bc10b871733cc7b33c97effe60
sagar bansal <bansalsagar13@gmail.com>
2025-04-17 18:37:30 +0530

    project structure and model mapper configuration

--- src/main/java/com/example/HotelBooking/HotelBookingApplication.java
+++ src/main/java/com/example/HotelBooking/HotelBookingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotelBookingApplication {
+public class 	HotelBookingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelBookingApplication.class, args);
