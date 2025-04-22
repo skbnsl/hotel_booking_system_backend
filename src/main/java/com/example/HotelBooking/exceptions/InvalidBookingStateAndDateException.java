b73e2eec97c6556b9ba19891d5b79e0249620961
sagar bansal <bansalsagar13@gmail.com>
2025-04-22 19:08:49 +0530

    Exceptions

---
+++ src/main/java/com/example/HotelBooking/exceptions/InvalidBookingStateAndDateException.java
@@ -0,0 +1,9 @@
+package com.example.HotelBooking.exceptions;
+
+public class InvalidBookingStateAndDateException extends RuntimeException{
+
+    public InvalidBookingStateAndDateException(String message){
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/example/HotelBooking/exceptions/InvalidCredentialException.java
@@ -0,0 +1,9 @@
+package com.example.HotelBooking.exceptions;
+
+public class InvalidCredentialException extends RuntimeException{
+
+    public InvalidCredentialException(String message){
+        super(message);
+    }
+
+}
