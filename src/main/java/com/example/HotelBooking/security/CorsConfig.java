12c273e8bf3de5c8f14fe4372f9de97845cf131a
sagar bansal <bansalsagar13@gmail.com>
2025-04-23 19:12:37 +0530

    cors config

--- src/main/java/com/example/HotelBooking/security/CorsConfig.java
+++ src/main/java/com/example/HotelBooking/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.HotelBooking.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Bean
     public WebMvcConfigurer webMvcConfigurer(){
         return new WebMvcConfigurer() {
             @Override
