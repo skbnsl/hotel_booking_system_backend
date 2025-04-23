6fcb44d0cee408550ce508dbf40be62820fcf2bc
sagar bansal <bansalsagar13@gmail.com>
2025-04-23 19:12:08 +0530

    cors config

---
+++ src/main/java/com/example/HotelBooking/security/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.HotelBooking.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    public WebMvcConfigurer webMvcConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET","POST","PUT","DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+
+}
